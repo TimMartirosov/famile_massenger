41f7635bf60f1532d3ca097d1001978ee7b4d423
TimMartirosov <Tymur.martirosov@nure.ua>
2021-10-10 02:28:58 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:famile_massenger/src/loginScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FssengerApp());
+}
+
+class FssengerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Sign in with Pnone number",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: LoginScreen(),
+    );
+  }
+}
